4e6f09a5b3f4330246aff2abe49e729b8d0b8146
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2023-05-23 15:42:58 +0200

    Added right button on the navigation bar with the list of websites

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -8,7 +8,7 @@
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
-    var webView: WKWebView!
+    private var webView: WKWebView!
     
     override func loadView() {
         webView = WKWebView()
@@ -19,8 +19,35 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://www.apple.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc private func openTapped() {
+        let alertController = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        alertController.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        // only for iPad
+        if #available(iOS 16.0, *) {
+            alertController.popoverPresentationController?.sourceItem = navigationItem.rightBarButtonItem
+        } else {
+            alertController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        }
+        
+        present(alertController, animated: true)
+    }
+    
+    private func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 }
